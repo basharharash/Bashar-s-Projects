57a9f98c040707e8ac22fdaf254e626079b57343
basharharash <90409951+basharharash@users.noreply.github.com>
2022-05-03 19:58:47 -0600

    Super Lario

---
+++ Super Lario/Assets/Scripts/Player scripts/player_fire.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_fire : MonoBehaviour {
+
+    public GameObject bullet;
+
+    void FixedUpdate() {
+        fire();
+    }
+
+    void fire() {
+        if (Input.GetKeyDown(KeyCode.K)) {
+            // make copies of the bullet
+            GameObject bullet_clone = Instantiate(bullet, transform.position, Quaternion.identity);
+            bullet_clone.GetComponent<fire_bullet>().Speed *= transform.localScale.x;
+        }
+    }
+}
